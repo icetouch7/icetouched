b6e8421d0fb92360657fc840a15b2e169a9294b0
icetouch7 <34977039+icetouch7@users.noreply.github.com>
2018-04-09 23:18:23 +0800

    databasestart

--- index.py
+++ index.py
@@ -1,20 +1,46 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,session,redirect,url_for,flash
 from flask_bootstrap import Bootstrap
-from flask_wtf import Form
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField
 from wtforms.validators import DataRequired
+import os
 
-class Nameform(Form):
+class Nameform(FlaskForm):
     name = StringField('What is s your name',validators = [DataRequired()])
     submit = SubmitField('Submit')
 
 
-
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'tell me why'
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+bootstrap = Bootstrap(app)
 
+
+
+#数据库系列database
+
+
+db = SQLAlchemy(app)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(64),unique = True)
+    users = db.relationship('User',backref = 'role')
+    def __repr__(self):
+        return '<Role %r>' % self.name
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(64),unique = True, index = True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 
@@ -31,9 +57,10 @@ def index():
     name = None
     form = Nameform()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html',form = form , name = name)
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    flash('Hello Everyone')
+    return render_template('index.html',form = form , name = session.get('name'))
 #错误信息
 @app.errorhandler(404)
 def page_not_found(e):
